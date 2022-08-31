9f0969207eafa6d962f274bf9b4af48410554a3a
Hay I'am Inces <74086121+fitri612@users.noreply.github.com>
2022-09-01 00:02:42 +0700

    Update Services and Footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 
 import logo from './logo.svg';
 import './App.css';
-import { NavBar } from "./components/NavBar";
-import { Banner } from "./components/Banner";
+import { NavBar } from "./components/Navbars/NavBar";
+import { Banner } from "./components/Banners/Banner";
+import { Footer } from './components/Footers/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import  Services  from './components/Service/Services';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <Services />
+      <Footer />
     </div>
   );
 }
